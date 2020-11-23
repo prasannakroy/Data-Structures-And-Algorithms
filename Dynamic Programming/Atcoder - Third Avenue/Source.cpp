d4820f05ba085431ed6ca9162df02388fb0752d8
Prasanna Kumar <prasannakroy2000@gmail.com>
2020-11-23 21:35:57 +0530

    DSA

--- Dynamic Programming/Atcoder - Third Avenue/Source.cpp
+++ Dynamic Programming/Atcoder - Third Avenue/Source.cpp
@@ -16,13 +16,32 @@ using namespace std;
 #define sumr(l, r) sum(r) - sum(l)
 
 void solve() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	double dp[101][101][101];
+	memset(dp, 0, sizeof dp);
+
+	function<double(int, int, int)> compute = [&] (int a, int b, int c) {
+		if (a == 100 or b == 100 or c == 100) return 0.0;
+		if (dp[a][b][c] > 0.0) return dp[a][b][c];
+
+		dp[a][b][c] = 0;
+
+		if (a) dp[a][b][c] += (((double)a / (a + b + c)) * (compute(a + 1, b, c) + 1));
+		if (b) dp[a][b][c] += (((double)b / (a + b + c)) * (compute(a, b + 1, c) + 1));
+		if (c) dp[a][b][c] += (((double)c / (a + b + c)) * (compute(a, b, c + 1) + 1));
+		return dp[a][b][c];
+	};
+
+	cout << fixed << setprecision(7) << compute(a, b, c) << "\n";
 }
 
 signed main() {
     // auto start = high_resolution_clock::now();
-    #ifndef ONLINE_JUDGE 
-        __AnAkLuSmOs    
-    #endif
+    // #ifndef ONLINE_JUDGE 
+    //     __AnAkLuSmOs    
+    // #endif
 
     __AcHiLlEs
 
